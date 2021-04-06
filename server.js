e2aec4a103afcc11b810273fd7b2c9e9e96f743f
Israa-Ireqat <israa.ireqat333@gmail.com>
2021-04-06 21:12:49 +0300

    almost done

--- server.js
+++ server.js
@@ -1,86 +1,112 @@
 'use strict';
 
-//Dotenv (Read our inviroment variable)
-require('dotenv').config();
-
 // Application dependencies /express & cors
 const express = require('express');
 //Origin resource sharing
 const cors = require('cors');
 //request library
-const superagent = require('superagent');
-
+const server = express();
 //Application setup
 const PORT = process.env.PORT || 3000;
-const server = express();
+//Dotenv (Read our inviroment variable)
+require('dotenv').config();
 server.use(cors());
-
+const superagent = require('superagent');
 /////////////////////////
 ///// The routes ///////
 ///////////////////////
 
-server.get('/', routeHandeler);
-server.get('/location',locationHandeler);
-server.get('/weather',weatherHandeler);
-server.get('*',notFoundHandeler);
+server.get('/', routeHandler);
+server.get('/location',locationHandler);
+server.get('/weather',weatherHandler);
+server.get('/parks', parksRouteHandler);
+server.get('*',notFoundHandler);
 
 ///////////////////////////////
 //// Handeling Functions  ////
 /////////////////////////////
 
-function routeHandeler(req,res){
+function routeHandler(req,res){
   res.send('The server is working , Great job !');
 };
-////http://localhost:3000/location?city=Lynwood
-function locationHandeler (req, res) {
-//get the data from Api server (locationIQ)
-//send a request using superagent library (request URL && the key)
-let cityName = req.query.city;
-let  locationKey = 'pk.9655e2b3c22302ad4cf8df3a11cb2d40';
-let locationURL = `https://eu1.locationiq.com/v1/search.php?key=${locationKey}&q=${cityName}&format=json`;
 
-  //let locationData = require('./data/location.json');
-  //   console.log('server.get   locationData', locationData);
-  let cityData = new Place(locationData);
-  res.send(cityData);
-};
-//http:localhost:3000/weather
-function weatherHandeler (req, res) {
-  let getWeatherData = require('./data/weather.json');
+function locationHandler (req, res) {
+let cityQuery= req.query.city;
+let key = process.env.LOCATION_KEY;
+let locationURL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${cityQuery}&format=json`;
 
-  getWeatherData.data.forEach((item, index) => {
-    let description = getWeatherData.data[index].weather.description;
-    let vDate = getWeatherData.data[index].valid_date;
-    let cityWeather = new Weather(description, vDate);
+
+superagent.get(locationURL).then(geoData => {
+  let gData = geoData.body;
+ let locationInstance = new Place(cityQuery, gData);
+  res.send(locationInstance);
+});
+
+}
+
+function weatherHandler (req, res) {
+  let cityQuery = req.query.search_query;
+  let key = process.env.WEATHER_KEY;
+  let weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityQuery}&key=${key}`;
+  superagent.get(weatherUrl).then(weatherData => {
+    let wData = weatherData.body;
+    let items = wData.data.map((item, index) =>{
+     let description = item.weather.description;
+     let validDate = item.valid_date;
+     return new Weather(description , validDate);
+    } );
+    res.send(items);
   });
-  res.send(Weather.all);
 };
-//for not found
-function notFoundHandeler (req, res){
-  let errObject = {
-    status: 500,
-    responseText: 'Sorry, something went wrong',
+
+
+function parksRouteHandler(req, res) {
+  // https://developer.nps.gov/api/v1/parks?parkCode=abcd&limit=50
+  let cityQuery = req.query.search_query;
+  let key = process.env.PARKS_KEY;
+  let parkURL = `https://developer.nps.gov/api/v1/parks?q=${cityQuery}&api_key=${key}`;
+  superagent.get(parkURL).then(parksData => {
+    let arr = parksData.body.data.map((item, index) => {
+      return new Park(item);
+    });
+    res.send(arr);
+  });
+}
+
+// for not found
+// function notFoundHandler (req, res){
+//   res.status(404).send('Sorry some thing went wrong');
+// };
+function notFoundHandler(req,res){
+  let errorObject = {
+      status: 500,
+      resText : 'Error 500 , Not Found'
   };
-  res.status(500).send(errObject);
-};
+  res.status(500).send(errorObject);
+}
 
 /////////////////////
 ////constructors////
 ///////////////////
 
-const Place = function (locationData) {
-  this.search_query = 'Lynwood';
-  this.formatted_query = locationData[0].display_name;
-  this.latitude = locationData[0].lat;
-  this.longitude = locationData[0].lon;
+const Place = function (cityName,geoData) {
+  this.search_query = cityName;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
 };
 const Weather = function (desc, dat) {
   this.forecast = desc;
   this.time = dat;
-  Weather.all.push(this);
 };
-Weather.all = [];
 
+const Park = function (parksData) {
+  this.name = parksData.fullName;
+  this.address = `${parksData.addresses[0].line1}, ${parksData.addresses[0].stateCode}, ${parksData.addresses[0].city}`;
+  this.fee = parksData.entranceFees[0].cost;
+  this.description = parksData.description;
+  this.url = parksData.url;
+};
 // listen to the server
 server.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
