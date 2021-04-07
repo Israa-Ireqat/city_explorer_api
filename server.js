aabf38b0f98da55975a1b1308560cb7bffb2f145
Israa-Ireqat <israa.ireqat333@gmail.com>
2021-04-07 15:07:49 +0300

    lab

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const pg = require('pg');
 const server = express();
 const superagent = require('superagent');
 const PORT = process.env.PORT || 3000;
-const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL 
+    //ssl: { rejectUnauthorized: false } 
+});
 
 server.use(cors());
 server.get('/', routeHandeler);
@@ -109,4 +111,3 @@ function Location (cityName,geoData) {
         console.log(`Listening on PORT ${PORT}`);
       });
     });
-    
